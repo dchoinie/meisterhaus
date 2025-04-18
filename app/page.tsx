78760ea639f617e6bc0b0fd112c6049d0b224c3c
Dan Choiniere <dchoinie@gmail.com>
2025-04-18 08:19:51 -0500

    xx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Hero from "./custom_components/home/hero";
 import About from "./custom_components/home/about";
 import Rooms from "./custom_components/home/rooms";
